8fdaf90caa62d246a653b7461fc48a1e5829b373
yangqiang <1225807604@qq.com>
2021-04-28 22:23:06 +0800

    add build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ go mod vendor
 cp -n ./etc/app.ini.example ./etc/app.ini
 
 sysOS=$(uname -s)
-# shellcheck disable=SC2154
+
 echo 开始构建版本
 if [ "$sysOS" == "Darwin" ] ; then
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o micro-mall-api-darwin-amd64 main.go
