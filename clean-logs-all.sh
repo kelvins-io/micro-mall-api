226584c5195abaae5fe73087fbdc9921cfa6e8bd
kelvins-io <mybaishati@gmail.com>
2021-09-01 16:54:37 +0800

    stop shell rm nohub.out

--- clean-logs-all.sh
+++ clean-logs-all.sh
@@ -24,6 +24,7 @@ function loopPathCleanLogs() {
       cd "$file" || exit
       echo "=> $file"
       rm -rf logs/
+      rm -rf nohup.out
       cd ../
   done
 }
--- stop.sh
+++ stop.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
 ./micro-mall-api -s stop
 rm -rf micro-mall-api
-rm -rf nohup.out
