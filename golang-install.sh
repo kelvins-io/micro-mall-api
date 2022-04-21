5f383f593cc34a18b77a19f962634ff6632e1f3b
kelvins-io <mybaishati@gmail.com>
2022-04-22 06:44:43 +0800

    fix golang-install

--- golang-install.sh
+++ golang-install.sh
@@ -1,16 +1,16 @@
 wget https://dl.google.com/go/go1.16.15.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.16.15.linux-amd64.tar.gz
 # shellcheck disable=SC2232
-sudo export GOROOT=/usr/local/go
+export GOROOT=/usr/local/go
 # shellcheck disable=SC2232
-sudo export GOBIN="$GOROOT"/bin
+export GOBIN="$GOROOT"/bin
 # shellcheck disable=SC2232
-sudo export GOPROXY=https://goproxy.cn,https://goproxy.io,direct
+export GOPROXY=https://goproxy.cn,https://goproxy.io,direct
 # shellcheck disable=SC2232
 cd ~ || exit
 mkdir go
 # shellcheck disable=SC2232
-sudo export GOPATH=~/go
+export GOPATH=~/go
 # shellcheck disable=SC2232
-sudo export PATH="$PATH":"$HOME"/bin:"$GOBIN":"$GOROOT"
-echo go version
+export PATH="$PATH":"$HOME"/bin:"$GOBIN":"$GOROOT"
+go version
